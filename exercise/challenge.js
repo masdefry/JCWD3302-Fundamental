72352e531531baf87e2f1619e4910ebdb30a9776
masdefry <muhammaddefryan@gmail.com>
2025-06-05 09:19:53 +0700

    add challenge

---
+++ exercise/challenge.js
@@ -0,0 +1,42 @@
+/*
+    01 - Hitung Diskon Belanja
+    Buat function untuk menghitung diskon berdasarkan total belanja, dengan ketentuan:
+    - Jika total belanja lebih dari 500.000, diskon yang didapat 20%
+    - Jika total belanja lebih dari 300.000, diskon yang didapat 10%
+    - Jika total belanja lebih dari 150.000, diskon yang didapat 5%
+    - Selain ketentuan diatas, maka tidak mendapatkan diskon belanja
+*/
+
+/*
+    02 - Hitung Jumlah Bilangan Ganjil dan Genap
+    Buat function untuk menghitung jumlah bilangan ganjil maupun genap dari deret array:
+
+    ex. Input   : [100, 200, 3, 5, 6]
+        Output  : Jumlah Bilangan Genap = 3, Jumlah Bilangan Ganjil = 2
+*/
+
+/*
+    03 - Palindrome Array
+    Buat function untuk mengecek apakah input dari sebuah array merupakan palindrome (urutan sama dari depan ke belakang)
+
+    ex. Input   : [100, 5, 3, 5, 100]
+        Output  : Palindrome
+
+        Input   : [1, 2, 3, 2, 5]
+        Output  : Not Palindrome
+
+        Input   : [1, 2, 2, 1]
+        Ouput   : Palindrome
+*/
+
+/*
+    04 - Pattern
+    Buat function untuk mencetak garis miring dengan pattern bintang
+
+    ex. Input   : 5
+        Output  :      *
+                      *
+                     *
+                    *
+                   *  
+*/
